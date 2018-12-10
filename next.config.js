31afc131e5c93c50d210221503377c21ab8dcabb
Jesus <jsfuentes@alu.ucam.edu>
2018-12-10 10:35:14 +0100

    Updated deps, fixedd next config, added temporary icon

--- next.config.js
+++ next.config.js
@@ -4,11 +4,11 @@
 const { PHASE_PRODUCTION_SERVER } =
   process.env.NODE_ENV === 'development'
     ? {}
-    : !process.env.NOW
+    : !process.env.NOW_REGION
       ? require('next/constants')
       : require('next-server/constants');
 
-module.exports = (phase, { defaultConfig }) => {
+module.exports = (phase) => {
   if (phase === PHASE_PRODUCTION_SERVER) {
     // Config used to run in production.
     return {};
